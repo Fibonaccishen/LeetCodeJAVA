1486503d54524d233ee600b8c62662b9de760674
shen <1025695049@qq.com>
2020-10-12 15:22:39 +0800

    9.30 leetcode83

--- src/JAVA/Exercise10.java
+++ src/JAVA/Exercise10.java
@@ -1,4 +1,40 @@
 package JAVA;
-
+//83. 删除排序链表中的重复元素
+//leetcode83
 public class Exercise10 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+
+    class Solution {
+        public class ListNode {
+            int val;
+            ListNode next;
+
+            ListNode(int x) {
+                val = x;
+            }
+        }
+
+        public ListNode middleNode(ListNode head) {
+            ListNode a=head;
+            while(a!=null){
+                if(a.next==null)break;
+                if(a.val==a.next.val){
+                    a.next=a.next.next;
+                }
+                if(a.next==null)break;
+
+                if(a.val!=a.next.val)
+                a=a.next;
+            }
+            return head;
+        }
+    }
 }
+
