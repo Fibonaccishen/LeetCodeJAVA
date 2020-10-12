53d8e91d23b668a39e4f62bfb0d35f56c667eb3a
shen <1025695049@qq.com>
2020-10-12 18:51:49 +0800

    10.12栈：删除最外层的括号

--- src/JAVA/Exercise14.java
+++ src/JAVA/Exercise14.java
@@ -1,4 +1,51 @@
 package JAVA;
 
+import java.util.Arrays;
+
+//leetcode1021. 删除最外层的括号
+//栈
+
+//既然是栈的题目，就要用栈的思路来思考问题
+//我自己的思路仍然是直观的在思考
+//后面的大神思路很棒
 public class Exercise14 {
+    public static String removeOuterParentheses(String S) {
+        char[] bracket = new char[S.length()];
+        int frontBra = 0;//前括号个数
+        int backBra = 0;//后括号个数
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '(') {
+                if(frontBra!=0) {
+                    bracket[i] = S.charAt(i);
+                }
+                frontBra++;
+            }
+            if (S.charAt(i) == ')') {
+                backBra++;
+                if (frontBra == backBra) {
+                    frontBra = 0;
+                    backBra = 0;
+                } else
+                    bracket[i] = S.charAt(i);
+            }
+        }
+        String str= new String(bracket);
+        str = str.replaceAll("[\u0000]", "");
+        return str;
+    }
+
+//leetcode上的大神做法
+//    StringBuilder sb = new StringBuilder();
+//    int level = 0;
+//        for (char c : S.toCharArray()) {
+//        if (c == ')') --level;
+//        if (level >= 1) sb.append(c);
+//        if (c == '(') ++level;
+//    }
+//        return sb.toString();
+
+
+    public static void main(String[] args) {
+        System.out.println(removeOuterParentheses("((()())(()))").length());
+    }
 }
