3a2adfe70bd47a344ecd6f6fe49569c37ea03e83
shen <1025695049@qq.com>
2020-10-12 15:25:15 +0800

    9.29

--- src/JAVA/Exercise8.java
+++ src/JAVA/Exercise8.java
@@ -1,4 +1,40 @@
 package JAVA;
 
+//从尾到头打印链表
+//leetcode 剑指 Offer 06.
 public class Exercise8 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public class ListNode {
+            int val;
+            ListNode next;
+
+            ListNode(int x) {
+                val = x;
+            }
+        }
+
+        public int[] reversePrint(ListNode head) {
+            int[] arr = new int[10000];
+            int length = 0;
+            ListNode tem = head;
+            while (tem != null) {
+                arr[length] = tem.val;
+                length++;
+                tem = tem.next;
+            }
+            int[] res = new int[length];
+            for (int i = 0; i < length; i++) {
+                res[i] = arr[length - i - 1];
+            }
+            return res;
+        }
+    }
 }
--- src/JAVA/Exercise9.java
+++ src/JAVA/Exercise9.java
@@ -1,4 +1,69 @@
 package JAVA;
+//两个链表的第一个公共节点
+//leetcode 剑指 Offer 52.
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
 public class Exercise9 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            if (headA == null || headB == null) return null;
+            ListNode a = headA;
+            ListNode b = headB;
+            while (a != b) {
+                a = (a == null) ? headB : a.next;
+                b = (b == null) ? headA : b.next;
+            }
+            return a;
+        }
+    }
+
+    //    public class Solution {
+//        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//            理解错误题意之后的复杂做法
+//            ListNode a = headA;
+//            ListNode b = headB;
+//            while (a != null) {
+//                while (b != null) {
+//                    if (b.val == a.val) {
+//                        //判断是否后面序列全都相等
+//                        ListNode copyA = a;
+//                        ListNode copyB = b;
+//                        while (copyA != null && copyB != null) {
+//                            if (copyA == null || copyB == null || copyA.val != copyB.val) {
+//                                break;
+//                            }
+//                            if (copyA.val == copyB.val) {
+//                                copyA = copyA.next;
+//                                copyB = copyB.next;
+//                            }
+//                            if (copyA == null && copyB == null) return a;
+//                        }
+//                    }
+//                    b = b.next;
+//                }
+//                a = a.next;
+//            }
+//            return null;
+//        }
+//    }
 }
