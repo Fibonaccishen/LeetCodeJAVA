971bc01273a6a53a4259de2871d493a837ae855f
shen <1025695049@qq.com>
2020-10-13 20:48:57 +0800

    10.13两个栈实现队列

--- src/JAVA/Exercise15.java
+++ src/JAVA/Exercise15.java
@@ -1,4 +1,28 @@
 package JAVA;
 
-public class Exercise15 {
+//剑指 Offer 09. 用两个栈实现队列
+//leetcode
+import java.util.LinkedList;
+
+//这道题对栈和队列的理解很有帮助，这一次并没有实现，看的别人的代码
+//但是着重是理解栈和队列的特性
+public class CQueue {
+    LinkedList<Integer> A, B;
+
+    public CQueue() {
+        A = new LinkedList<Integer>();
+        B = new LinkedList<Integer>();
+    }
+
+    public void appendTail(int value) {
+        A.addLast(value);
+    }
+
+    public int deleteHead() {
+        if (!B.isEmpty()) return B.removeLast();
+        if(A.isEmpty())return -1;
+        while (!A.isEmpty())
+            B.addLast(A.removeLast());
+        return B.removeLast();
+    }
 }
