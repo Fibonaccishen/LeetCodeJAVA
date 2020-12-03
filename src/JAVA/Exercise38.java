735a0b1fba17bf6c4d0562c2b5c08313b1644100
shen <1025695049@qq.com>
2020-12-03 21:17:49 +0800

    两数之和（链表）

---
+++ src/JAVA/Exercise38.java
@@ -0,0 +1,103 @@
+package JAVA;
+
+import java.util.List;
+
+/**
+ * @Author Shen Fangzhi
+ * @Description:
+ * @Date: Created in 19:32 2020/12/3
+ * @Modified By:
+ **/
+
+
+public class Exercise38 {
+
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        ListNode first = result;
+
+        ListNode list1 = l1;
+        ListNode list2 = l2;
+
+        int advance = 0;
+
+        while (list1 != null && list2 != null) {
+
+            int list1Num = list1.val;
+            int list2Num = list2.val;
+            int temp = list1Num + list2Num + advance;
+
+            int curVal = temp % 10;
+
+            result.val = curVal ;
+
+            if (list1.next != null || list2.next != null) {
+                result.next = new ListNode();
+                result = result.next;
+            }
+
+            advance = temp / 10;
+
+            list1 = list1.next;
+            list2 = list2.next;
+        }
+
+        while (list1 == null && list2 != null) {
+
+            int temp = list2.val + advance;
+            int curVal = temp % 10;
+
+            result.val = curVal;
+
+            if (list2.next != null) {
+                result.next = new ListNode();
+                result = result.next;
+            }
+
+            advance = temp / 10;
+
+            list2 = list2.next;
+        }
+
+        while (list1 != null && list2 == null) {
+
+            int temp = list1.val + advance;
+            int curVal = temp % 10;
+
+            result.val = curVal;
+
+            if (list1.next != null) {
+                result.next = new ListNode();
+                result = result.next;
+            }
+
+            advance = temp / 10;
+
+            list1 = list1.next;
+        }
+
+        if(advance == 1){
+            result.next = new ListNode(1);
+        }
+
+        return first;
+    }
+}
